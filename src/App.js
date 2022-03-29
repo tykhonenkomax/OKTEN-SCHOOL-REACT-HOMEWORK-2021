f5bc3b18882525fb0be06d6133ad01dc66a19e37
Max <95238323+tykhonenkomax@users.noreply.github.com>
2022-03-29 20:27:58 +0300

    Lesson2 lecture

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import UserComponents from "./myFolder/components/Users/UserComponents";
 
 function App() {
+
   return (
-    <div className="App">
+    <div>
 
     </div>
   );
