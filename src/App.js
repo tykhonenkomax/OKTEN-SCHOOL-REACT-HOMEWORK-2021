d3e501d4f7af4dcd08c074a62da81de6737e6ba8
Max <95238323+tykhonenkomax@users.noreply.github.com>
2022-04-02 00:15:51 +0300

    Lesson3 Лекция ЖОПА!!!

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
 
 import './App.css';
-import {useEffect, useState} from "react";
-import UserComponents from "./myFolder/components/Users/UserComponents";
-import {getPosts, getUsers} from "./myFolder/DirtyСode/DirtyCode";
+
 
 
 
 function App() {
 
-let [x,y]=useState([])
-
-useEffect(()=>{
-getUsers().then(value => y([...value]));
-
-},[])
-
   return (
     <div>
-      <div>
-        {
-            x.map(value => <UserComponents key={value.id} item={value}/>)
 
-        }
-      </div>
 
     </div>
   );
--- src/myFolder/components/Users/UserComponents.js
+++ src/myFolder/components/Users/UserComponents.js
@@ -1,27 +1,9 @@
-function UserComponents(props) {
+function UserComponents() {
 
-let {item:{ id, name, username, email,address}}=props;
+
     return(
         <div>
-            <p><strong>Id №:</strong> {id} <strong> <br/>
-                Name:</strong> {name} <br/>
-                <strong>Surname:</strong> {username} <br/>
-                <strong>Email:</strong> {email} <br/>
-                <strong>ADDRESS:</strong> <br/>
-                <strong>Street:</strong> {address.street} <br/>
-                <strong>Suite:</strong> {address.suite} <br/>
-                <strong>City:</strong> {address.city} <br/>
-                <strong>ADDRESS:</strong> <br/>
-                <strong>Street:</strong> {address.street} <br/>
-                <strong>Suite:</strong> {address.suite} <br/>
-                <strong>City:</strong> {address.city} <br/>
-                <strong>Zipcode:</strong> {address.zipcode} <br/>
-                <strong>GEO:</strong> <br/>
-                <strong>lat:</strong> {address.geo.lat} <br/>
-                <strong>lng:</strong> {address.geo.lng} <br/>
 
-
-            </p>
 
         </div>
     );
