ec18dd1f4e8b612af3514bc7593ccf22f28fe457
Max <95238323+tykhonenkomax@users.noreply.github.com>
2022-04-14 11:53:21 +0300

    Lesson 3 Part 1 (CRUD)

--- src/Components/FormComponents/FormComponents.js
+++ src/Components/FormComponents/FormComponents.js
@@ -1,25 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useForm} from "react-hook-form";
+import {CRUDCarServices} from "../../Services";
 
 const FormComponents = () => {
 
-    const {register,handleSubmit,reset}=useForm();
+    const [errorResponce, setErrorResponce] = useState({})
 
-    const infoFromInput =(value)=>{
-        console.log(value)
+    const {
+        register, handleSubmit, watch, formState: {errors}
+    } = useForm();
+
+    const infoFromInput = async (car) => {
+        try {
+            const newCar = await CRUDCarServices.createCars(car)
+            console.log(newCar)
+
+
+        } catch (error){
+        setErrorResponce(error.response.data)
     }
+}
+
 
     
     return (
         <div>
             <form onSubmit={handleSubmit(infoFromInput)}>
                 <div><label>Model: <input type='text' {...register('model')}/></label></div>
+                {errorResponce.model && <span>{errorResponce.model[0]}</span>}
                 <div><label>Price: <input type='text' {...register('price', {valueAsNumber:true})}/></label></div>
+                {errorResponce.price && <span>{errorResponce.price[0]}</span>}
                 <div><label>Year: <input type='text'{...register('year',{valueAsNumber:true} )}/></label></div>
+                {errorResponce.year && <span>{errorResponce.year[0]}</span>}
                 <button>PUSH MY TRA LA LA</button>
             </form>
         </div>
-    );
+    )
 };
 
 export default FormComponents;
--- src/Services/CRUDCarServices.js
+++ src/Services/CRUDCarServices.js
@@ -7,7 +7,7 @@ const CRUDCarServices={
     createCars:(car)=>axiosServices.post(links.cars, car),
     deleteCarsById:(id)=>axiosServices.delete( `${links.cars}/${id}`),
     getCarsById:(id)=>axiosServices.get(`${links.cars}/${id}`),
-    refreshObjectCars:(id)=>axiosServices.put(`${links.cars}${id}`)
+    refreshObjectCars:(id, car)=>axiosServices.put(`${links.cars}${id}`, car)
 
 }
 export {CRUDCarServices}
