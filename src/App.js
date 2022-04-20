af6f22f8f6fdc9133c30f6aad40019ced02c33e5
Max <95238323+tykhonenkomax@users.noreply.github.com>
2022-04-20 23:38:09 +0300

    MASTER

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
 
 import './App.css';
 
-import {Routes, Route, Navigate} from 'react-router-dom';
-
-import {MainLayouts} from "./Layours/MainLayouts";
-import {HomePage} from "./Pages/HomePage/HomePage";
-import {UsersPage} from "./Pages/UsersPage/UsersPage";
-import {PostsPage} from "./Pages/PostsPage/PostsPage";
-import {AboutPage} from "./Pages/About Page/AboutPage";
-import {NotFoundPage, SinglePage} from "./Pages";
-
 function App() {
   return (
-<Routes>
-    <Route path={'/'} element={<MainLayouts/>}>
-      <Route index element={<Navigate to={'/home'}/>}/>
-  <Route path={'home'} element ={<HomePage/>}/>
-  <Route path={'users'} element={<UsersPage/>}/>
-  <Route path={'posts'} element={<PostsPage/>}>
-    <Route path={':id'} element={<SinglePage/>}/>
-    </Route>
-  <Route path={'about'} element={<AboutPage/>}/>
-  <Route path={'*'} element={<NotFoundPage/>}/>
+<div>
 
-  </Route>
-
-</Routes>
+</div>
   );
 }
 
