9ee19d843cc18b901a74d02ba84e0039fccf8d45
Max <95238323+tykhonenkomax@users.noreply.github.com>
2022-04-24 12:39:31 +0300

    1

--- src/index.js
+++ src/index.js
@@ -3,11 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+
+
+
+function reducer () {
+return 'Hello'
+}
+
+
+let store= createStore(reducer);
+console.log(store.getState());
 
 ReactDOM.render(
     <React.StrictMode>
+
+        <Provider store={store}>
         <App />
+        </Provider>
+
     </React.StrictMode>,
+
     document.getElementById('root')
 );
 
